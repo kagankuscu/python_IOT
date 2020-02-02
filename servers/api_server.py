719717c7a65d3265b0cbfdb599ed3ea2ec8441e0
kagankuscu <kagan_kuscu@hotmail.com.tr>
2020-02-02 12:40:31 +0200

    Change url and test.json read function (temporary)

--- servers/api_server.py
+++ servers/api_server.py
@@ -1,36 +1,31 @@
 from flask import Flask, jsonify
+import json
 
 app = Flask(__name__)
 
-lights = {'lights':
-          {
-              'light1': False,
-              'light2': False,
-              'light3': False
-          }
-          }
+with open('/home/kagan/Documents/python/Raspberry_pi_iot/servers/test.json', 'r') as output:
+    data = json.load(output)
 
 
-@app.route('/lights')
-def get_lights():
-    return jsonify(lights)
+@app.route('/rooms')
+def get_rooms():
+    keys = list(dict.keys(data['rooms']))
+    return jsonify({'rooms': keys})
 
 
-@app.route('/lights/<string:light>')
-def get_ligth(light):
-    return jsonify({light: lights['lights'][light]})
+@app.route('/rooms/<string:room>')
+def get_room(room):
+    return jsonify({room: data['rooms'][room]})
 
 
-@app.route('/lights/<string:light>/on')
-def open_ligth(light):
-    lights['lights'][light] = True
-    return jsonify({light: lights['lights'][light]})
+@app.route('/rooms/<string:room>/<string:prop_of_room>')
+def get_lights_by_room(room, prop_of_room):
+    return jsonify({room: {prop_of_room: data['rooms'][room][prop_of_room] }})
 
 
-@app.route('/lights/<string:light>/off')
-def close_ligth(light):
-    lights['lights'][light] = False
-    return jsonify({light: lights['lights'][light]})
+@app.route('/rooms/<string:room>/<string:prop_of_room>/<string:prop_name>')
+def get_ligth_by_name(room, prop_of_room, prop_name):
+    return jsonify({room: {prop_of_room: {prop_name: data['rooms'][room][prop_of_room][prop_name] }}})
 
 
 if __name__ == "__main__":
